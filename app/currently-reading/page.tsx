92194ba2fcc09c8da115311b63cac67991e983ec
Anusha S <anushasridharan7@gmail.com>
2023-08-03 11:43:48 +0530

    initial layout

---
+++ app/currently-reading/page.tsx
@@ -0,0 +1,6 @@
+export default function Page() {
+    return <>
+      <h1 className="text-3xl font-bold">Currently Reading</h1>
+      <p>This section will show the current reads in the shelf - Share notes and highlights</p>
+    </>
+  }
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,6 @@
+export default function Page() {
+  return <>
+    <h1 className="text-3xl font-bold">Dashboard</h1>
+    <p>This dashboard will reflect the percentage of books of the different genres currently present in the library</p>
+  </>
+}
