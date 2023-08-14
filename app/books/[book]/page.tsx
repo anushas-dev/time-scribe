2930ec7157efc2491384705bb5ffedc240895414
Anusha S <anushasridharan7@gmail.com>
2023-08-14 23:52:12 +0530

    added book pages

---
+++ app/books/[book]/page.tsx
@@ -0,0 +1,12 @@
+export default function Page() {
+    return <>
+      <div className="body-font font-play">
+        <h1 className="text-3xl font-bold text-center">Book Name</h1>
+        <p>Book Description</p>
+        <p>Book Genre</p>
+        <p>Book Author</p>
+        <p>Book Highlights</p>
+        <p>Other Related or Similar Books</p>
+      </div>
+    </>
+  }
